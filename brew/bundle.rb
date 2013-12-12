8558448479b2894bcb4cda78032c08fcd2c13a45
Mick Staugaard <mick@staugaard.com>
2013-12-11 23:27:12 -0800

    brew updating and more packages

--- brew/bundle.rb
+++ brew/bundle.rb
@@ -2,5 +2,13 @@
 
 brewfile = File.expand_path('Brewfile', File.dirname(__FILE__))
 
+if `which brew`.empty?
+  puts "Installing home"
+  system 'ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"'
+else
+  puts "Updating homebrew"
+  system 'brew update'
+end
+
 puts "Installing homebrew packages"
 exec "brew bundle #{brewfile}"
