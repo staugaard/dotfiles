7c0af6e2433d378f4c560b5f058f22332a8679c6
Mick Staugaard <mick@staugaard.com>
2013-12-11 21:55:56 -0800

    sublime settings

--- sublime/setup.rb
+++ sublime/setup.rb
@@ -10,3 +10,19 @@ end
 puts "Symlinking #{ENV['HOME']}/bin/subl"
 system 'mkdir -p $HOME/bin'
 system 'ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/bin/subl'
+
+puts "Symlinking Sublime Text Settings"
+packages_path = "#{ENV['HOME']}/Library/Application Support/Sublime Text 3/Packages"
+system "mkdir -p '#{packages_path}/User'"
+['Staugaard.tmTheme', 'Preferences.sublime-settings'].each do |file|
+  system "ln -sf #{File.expand_path(file, File.dirname(__FILE__))} '#{packages_path}/User'"
+end
+
+{
+  'TrailingSpaces' => 'SublimeText/TrailingSpaces'
+}.each do |name, source|
+  unless File.exist?("#{packages_path}/#{name}/.git")
+    puts "Installing the #{name} Sublime Text Package"
+    system "git clone http://github.com/#{source} '#{packages_path}/#{name}'"
+  end
+end
