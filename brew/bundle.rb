1f24409775d4006e94a2f3a24fb7d23657a7ba13
Mick Staugaard <mick@staugaard.com>
2014-10-28 23:02:09 -0700

    Brewfile is gone

--- brew/bundle.rb
+++ brew/bundle.rb
@@ -8,9 +8,17 @@ if ENV['BOXEN_HOME']
 end
 
 if `which brew`.empty?
-  puts "Installing home"
+  puts "Installing homebrew"
   system 'ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"'
+else
+  puts "Upgrading homebrew"
+  system "brew update"
 end
 
 puts "Installing homebrew packages"
-exec "brew bundle #{brewfile}"
+system "brew install git"
+system "brew install htop"
+system "brew install ios-sim"
+system "brew install wget"
+system "brew install chruby"
+system "brew install ruby-install"
