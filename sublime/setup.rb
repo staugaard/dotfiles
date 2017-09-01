6fea09c92fac33a398b4a6bf6d18f9f36c4f47de
Mick Staugaard <mick@staugaard.com>
2017-09-01 10:09:03 -0700

    newer sublime

--- sublime/setup.rb
+++ sublime/setup.rb
@@ -2,7 +2,7 @@
 
 unless File.exist?('/Applications/Sublime Text.app')
   puts "Installing Sublime Text 3"
-  system 'hdiutil mount http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203047.dmg'
+  system 'hdiutil mount https://download.sublimetext.com/Sublime%20Text%20Build%203126.dmg'
   system 'cp -r /Volumes/Sublime\ Text/Sublime\ Text.app /Applications'
   system 'hdiutil unmount /Volumes/Sublime\ Text'
 end
