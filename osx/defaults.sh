7db28a5b41ea3b90d83b36b6bb5083683f12d228
Mick Staugaard <mick@staugaard.com>
2017-09-07 15:10:08 -0700

    more OSX settings

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
+# lots of good inspiration here:
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
+echo "Setting TouchPad Preferences"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+echo "Setting Keyboard Preferences"
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
 echo 'Setting Dock Preferences'
 defaults write com.apple.dock no-glass -boolean YES
 defaults write com.apple.dock orientation right
+defaults write com.apple.dock tilesize -int 46
+
 killall Dock
+killall SystemUIServer
