55bb1c0852a3cc9dc957ce8efecc8397bbde6a3a
Mick Staugaard <mick@staugaard.com>
2022-04-11 10:48:03 +1200

    Vend tweaks

--- oh-my-zsh/custom/plugins/vend/vend.plugin.zsh
+++ oh-my-zsh/custom/plugins/vend/vend.plugin.zsh
@@ -1,4 +1,6 @@
 if [ $(hostname) = "MickS-C02DF0TGMD6V" ]; then
   export GOPRIVATE=github.com/vend
+  export AWS_PROFILE=team-principals
+  source /Users/staugaard/code/vend/home/functions.sh
 fi
 
