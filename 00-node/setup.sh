b5429362a7cd279b6a8220f6d2eca76e9685a237
Mick Staugaard <mick@staugaard.com>
2023-07-05 20:56:29 +1200

    the condition here does not work

--- 00-node/setup.sh
+++ 00-node/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if ! which nvm &>/dev/null; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-  git checkout .zshrc
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+git checkout .zshrc
